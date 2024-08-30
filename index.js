d67322804956e06f56a9a9504940a1838a088eb9
filipecastro747 <castrofilipe449@gmail.com>
2024-08-30 16:01:41 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,272 @@
+
+//let qualsuaidade=prompt("qual sua idade")
+//let idademelhoramigo=prompt("idade melhor amigo")
+//console.log("sua idade é maior ou menor que a do seu melhor amigo?"+ qualsuaidade > idademelhoramigo)
+//console.log (qualsuaidade - idademelhoramigo)
+//let numeropar=prompt(" escreva um numero par")
+//const restoDaDivisão =  numeropar % 2 
+//console.log(restoDaDivisão) 
+//let nome=prompt( "insira seu nome " )
+//let cor=prompt(" insira sua cor favorita" )
+//const informacoes= " a cor favorita de " + nome + " é a cor " + cor
+//console.log (informacoes)
+//console.log ( ` nome ${nome} a cor favorita de  ${cor} `)
+//let frase=prompt("escrever uma frase")
+////console.log(frase.toUpperCase())
+//console.log (frase.replaceAll ("i" , "o") )
+//console.log(frase.length)
+//const racaDeCachorro =["Buldogue","pastor alemão","são bernardo","Chihuahua","Husky siberiano"] 
+//let racas= racaDeCachorro[Number (prompt("digite uma numero de 0 a 4"))]
+//console.log(racas)
+//let numeros =[1,2,3,4,5,6]
+//console.log(numeros.length)
+//numeros.push(7)
+//console.log(numeros)
+//numeros.splice(3, 2) 
+//console.log(numeros)
+//console.log(numeros.length)
+//let nomeDoUsuario = prompt("nome do usuario")
+//let emailDoUsuario = prompt("email do usuario")
+//console.log("o e-mail" + emailDoUsuario + "foi cadastrado com sucesso seja(o) bem vindo!" + nomeDoUsuario)
+//const comiDaSpreferidas = ["hamburger","batata", "alface","chocolate", "polvo"]
+//console.log(comiDaSpreferidas)
+//console.log("essas são minhas comidas preferidas" + comiDaSpreferidas ) 
+//const pegunteaousuario = prompt("qual sua comida preferida ?") 
+//comiDaSpreferidas [1] = pegunteaousuario
+//console.log(comiDaSpreferidas [0])
+//console.log(comiDaSpreferidas [1])
+//console.log(comiDaSpreferidas [2])
+//console.log(comiDaSpreferidas [3])
+//console.log(comiDaSpreferidas [4])
+//console.log(comiDaSpreferidas)
+//let array = [] 
+//let talistaDeTarefas = array
+//let tarefaUM = prompt("1 tarefa que voce realiza no dia a dia ")
+//let tarefaDOIS = prompt("  1  tarefas que voce realiza no dia a dia ")
+//let tarefaTRES = prompt("  1  tarefas que voce realiza no dia a dia ")
+//array [1] = tarefaUM
+//array [2] = tarefaDOIS
+//array [3] = tarefaTRES
+//console.log(talistaDeTarefas)
+//let indice = Number prompt (digi 
+//function imprimirOlaMundo() {
+    //console.log("ola mundo")
+//} 
+//imprimirOlaMundo()
+
+//let nome = function (nome) { 
+    //console.log(`olá ${nome}`)
+//}
+//nome()
+  //function nomeDOIS(nome1, nome2, nome3) {
+    //console.log(` ola ${nome1}, ${nome2} e ${nome3}`)
+   //}
+    //nome("caio", "Kauã", "Victor") 
+
+    //let soma =  (numero1, numero2) => { 
+    //return numero1 + numero2
+    //}
+    //let b=soma(3,4) 
+    //console.log(b) 
+    
+//const onepiece= {
+ // nome:'one piece egghead',
+  //ano: '2023',
+  //elenco :['carol','glauco','Wendel'],
+  //viuOuNao: true
+//} 
+//console.log(onepiece.nome)
+//console.log(onepiece.ano)
+//console.log(onepiece["elenco"])
+//console.log(onepiece["viuOuNao"])
+
+//const pessoa= {
+// nome:'filipe',
+ //idade: '15',
+ //GeneroMusical:'anime'
+
+//}
+//console.log( "o nome da pessoa é" + pessoa.nome + " ele tem " + pessoa.idade + " anos e gosta muito de " + pessoa.GeneroMusical)
+
+//onepiece.listaDePersonagens = ['nami','robin','chooper']
+
+//console.log(onepiece.listaDePersonagens[0]+ " : " + onepiece. elenco[0])
+//onepiece.elenco[0]='xuxa'
+
+//console.log(onepiece.elenco)
+///console.log(onepiece.nome)
+//console.log(onepiece.ano)
+//console.log(onepiece['elenco'])
+//console.log(onepiece['viuOuNao'])
+//console.log(onepiece['listaDePersonagens'])
+
+//let a  = Number (prompt ("digite um numero ?") )
+
+//let  b = Number (prompt ("digite outro numero ? ") )
+
+ //function comparador (num1,num2) {
+//if (num1 === num2) {
+//console.log("sucesso")  
+//}
+
+//else { 
+//console.log("diferente")
+//}
+//}
+
+//comparador(a, b)
+
+
+//let a  = Number (prompt ("digite um numero ?") )
+
+//let  b = Number (prompt ("digite outro numero ? ") )
+
+ //function comparador (num1,num2) {
+//if (num1 === num2) {
+//console.log("sucesso")  
+//}
+
+ //if  (num1===num2) {
+//console.log("igual")
+ //}else if (num1>num2)
+// console.log("maior")
+
+//console.log("menor")
+//}
+ //comparador(a,b)
+
+ //let pokemon = prompt("digete um pokemon")
+ //switch (pokemon) {
+  //case 'Bulbasauro':
+    //console.log('planta e veveno')
+   // break
+ // case  'Charmander':
+ //console.log('fogo')
+    //break
+  //case  ' Squirtle ':
+    //console.log('água')
+    //break
+
+  //default:
+ //console.log("O tipo não foi encontrado") 
+ //break
+ //}
+
+ //let ensino = prompt(" se tiver concluido o ensino medio" )
+ //let idade = Number (prompt("tiver 18 anos ou mais "))
+ //let cursando = prompt("não estiver cursando outra faculdade")
+ 
+ //function medio (ensino,idade,cursando){
+  //if (idade <= 18 && cursando === "sim" && cursando === "não"){
+    //console.log("você pode estudar na faculdade")
+  
+  //}
+  //else {
+   // console.log('você pode não estudar na faculdade')
+
+   
+// }
+//}
+//medio(idade, ensino,cursando)
+
+//function sonic() {
+  //let soma=0
+ // while(true){
+    //let numero= number (prompt("digite um numero ou 0 para sair"))
+    //if(numero===0)
+    //break
+  //}
+//}
+//soma=numero+soma
+//console.log(soma)
+
+//(sonic)
+
+//let numeros =  [10,20,30,40,50]
+//for(let numeros =  
+
+
+    
+    
+    
+     let abrigos= []   
+     
+    
+  
+  function  cadastra()  {
+
+    let nome = prompt ("qual é o nome do abrigo ?")
+    let telefone = prompt ("qual é o telefone do abrigo ?")
+    let endereço = prompt ("qual é o endereço do abrigo ?")
+    let capacidade = prompt ("qual é a capacidade de pessoas no abrigo ?")
+    let cidade = prompt ("qual é cidade do seu abrigo ?")
+  
+    const abrigo   = {     
+nome: nome,
+telefone: telefone,
+endereço: endereço,
+capacidade: capacidade,
+cidade: cidade
+
+    }   
+    abrigos.push(abrigo)
+    alert (" Cadastrado com sucesso")
+  }
+ 
+
+  function Listar() {
+    if(abrigos.length===0){
+    alert(" Não tem abrigos cadastrados")
+    } else {
+      for  (let i= 0; i < abrigos.length; i++){
+       const b = abrigos [i];
+        
+        alert(`====LISTAGEM DE ABRIGOS===\n nome ${b.nome}\n telefone ${b.telefone}\n endereço ${b.endereço}\n capacidade ${b.capacidade}\n cidade ${b.cidade}\n codigo ${i + 1}`)
+      
+      
+      }
+
+    }
+    
+  }
+
+  function Procurar() {
+  
+        let cidade = prompt("digite sua cidade").toLowerCase()
+     if(abrigos.length===0){
+         
+        }else{
+          for (let i= 0; i < abrigos.length; i++){
+          if(abrigos[i].cidade.includes(cidade)){
+            alert (`====LISTAGEM DE ABRIGOS===\n nome ${abrigos[i].nome}\n telefone ${abrigos[i].telefone}\n endereço ${abrigos[i].endereço}\n capacidade ${abrigos[i].capacidade}\n cidade ${abrigos[i].cidade}\n codigo ${i + 1}`)
+          } 
+          }
+        }
+      }
+let menu=0
+
+while (menu !== 4) {
+ let menu= prompt ("===== ABRIGOS  TEMPORÁRIOS =====\n 1 cadastre abrigo\n 2 Listar abrigos \n 3 Procurar abrigo \n 4 Sair")
+
+ switch (menu){
+  case'1': 
+  cadastra()
+  break
+  
+  case '2':
+    Listar()
+break
+
+case '3':
+   Procurar()
+break
+
+case '4':
+   
+  alert("muito obrigado por utilizar o programa!")
+break;
+default:
+ alert ("tente novamente!")
+
+ }
+}
